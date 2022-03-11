db63ff81cd8159d46af3c52d9fb79166c252dfe1
vandilsoncosta1 <101371663+Vanilson13@users.noreply.github.com>
2022-03-11 00:43:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask('app')
+
+@app.route('/')
+def hello_world():
+  return '<h1>Minha primeira rota! </h1>'
+
+  
+@app.route('/Minha primeira rota!')
+def hello():
+  return '<h1>Minha primeira rota! </h1>'
+
+app.run(host='0.0.0.0', port=8080)
+
+
